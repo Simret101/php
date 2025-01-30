b427653df3e90795151ebfcbe71f62fdb0067110
Simret <semretb74@gmail.com>
2025-01-30 12:39:23 +0300

    feat:add number

---
+++ number.php
@@ -0,0 +1,17 @@
+<?php
+
+// B I D M A S
+$age1=10;
+$age2 =20.5;
+$age1+=10;
+//increment and decrement
+echo $age1--;
+echo $age1;
+echo $age1++;
+echo $age1;
+echo $age1*=2;
+echo ceil($age2);
+echo floor($age2);
+echo pi();
+
+?>
