58635129a75f8411eb58521e1ff3bcc2d3b7440d
Simret <semretb74@gmail.com>
2025-01-30 11:29:44 +0300

    feat:add php starter

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo 'hello,simret';
+
+?>
+
+<!--embeding php inside html-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "hello,there world";?></h1>
+</body>
+</html>
