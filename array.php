5b75bbfca9d11223ac488fcae3e31c09c4a387c7
Simret <semretb74@gmail.com>
2025-01-30 14:13:22 +0300

    feat:add array

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+//indexed array
+$array1=[1,2,3,5];
+$array2=array(1,2,3,4);
+$array3=array_merge($array1,$array2);
+print_r($array3);
+//associative array
+$array1=["name"=>"simret","rose"=>"red"];
+print_r($array1);
+$array1["name"]="shalom";
+ echo $array2[0];
+
+
+
+
+?>
