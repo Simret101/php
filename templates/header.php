4be1c4a8a632d9f0d950b159524215821be719e0
Simret <semretb74@gmail.com>
2025-01-31 04:22:07 +0300

    feat:add header and footer

---
+++ templates/header.php
@@ -0,0 +1,24 @@
+
+<head>
+    
+    <title>S&S PIZZA</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+        .brand{
+            background:#cbb09c !important;
+        }
+        .brand-text{
+            color:#cbb09c !important;
+        }
+    </style>
+</head>
+<body class="grey lighten-4">
+    <nav class="white z-deepth=0">
+       
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">S&S PIZZA</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-mobile">
+            <li><a href="#" class="btn  brand z-depth-0">ADD A PIZZA</a></li>
+            </ul>
+        </div>
+    </nav>
